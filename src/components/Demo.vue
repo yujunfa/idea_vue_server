ddf12bd1c312aa41903f0af0c6384a1eceacbe30
yujunfa97 <yujunfa97@163.com>
2019-08-25 15:30:57 +0800

    配置好Bootstrap-Vue开发环境,编写了测试Demo

---
+++ src/components/Demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-button variant="primary">Hello</b-button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: ""
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Demo from '@/components/Demo'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/demo',
+      component: Demo
+    },
     {
       path: '/',
       name: 'HelloWorld',
