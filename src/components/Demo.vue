72e4926f51d224cbf82b630840b44942ee240057
yujunfa97 <yujunfa97@163.com>
2019-08-26 17:30:02 +0800

    配置好Bootstrap-Vue开发环境,编写了测试Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <b-button variant="primary">Hello</b-button>
+    <b-button @click="fun1" variant="primary">Hello</b-button>
   </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "",
+        methods: {
+            fun1() {
+                confirm('Hello World!');
+            }
+        }
     }
 </script>
 
